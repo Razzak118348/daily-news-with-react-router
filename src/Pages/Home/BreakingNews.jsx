ae49d73bad876d68133f0970278ffe115f9b68bc
razzak118348 <abdurrazzak118348@gmail.com>
2024-09-09 13:32:42 +0600

    final commit

--- src/Pages/Home/BreakingNews.jsx
+++ src/Pages/Home/BreakingNews.jsx
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 const BreakingNews = () => {
     return (
         <div className="flex bg-[#F3F3F3] font-poppins font-semibold rounded-lg text-lg p-3  mt-3">
-            <button className= "btn  btn-secondary bg-red-700 font-poppins text-white text-sm md:text-lg font-medium">Latest News</button>
+            <Link to={'https://www.nydailynews.com/'} className= "btn  btn-secondary bg-red-700 font-poppins text-white text-sm md:text-lg font-medium">Latest News</Link>
             <Marquee pauseOnHover={true} speed={90}>
-                hi thei slkdfjslkdfjwoejwehsoidfsiodfsdfjslkdfj
-                <Link  to={'/latestNews'} className="text-green-800 mx-4"> news of lateset</Link>
-                <Link to={'/latestNews'} className="text-green-800"> news of lateset</Link>
-                <Link to={'/latestNews'} className="text-green-800 mx-5"> news of lateset</Link>
+************Welcome to Daily news website .. We truset unfear and Publish true News ever
+                <Link  to={'https://www.nydailynews.com/'} className="text-green-800 mx-4"> Latest News</Link>
+
             </Marquee>
             {/* marquee theke marquee install kore then import kore marquee use korte hoy  */}
         </div>
