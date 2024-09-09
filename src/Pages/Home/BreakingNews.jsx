34846e0950b429adcb672f035b98516a2a3751c5
razzak118348 <abdurrazzak118348@gmail.com>
2024-09-09 13:44:59 +0600

    merquee

--- src/Pages/Home/BreakingNews.jsx
+++ src/Pages/Home/BreakingNews.jsx
@@ -5,7 +5,7 @@ const BreakingNews = () => {
         <div className="flex bg-[#F3F3F3] font-poppins font-semibold rounded-lg text-lg p-3  mt-3">
             <Link to={'https://www.nydailynews.com/'} className= "btn  btn-secondary bg-red-700 font-poppins text-white text-sm md:text-lg font-medium">Latest News</Link>
             <Marquee pauseOnHover={true} speed={90}>
-************Welcome to Daily news website .. We truset unfear and Publish true News ever
+************Welcome to Daily news website .. We truset truthfulness and Publish true News ever
                 <Link  to={'https://www.nydailynews.com/'} className="text-green-800 mx-4"> Latest News</Link>
 
             </Marquee>
