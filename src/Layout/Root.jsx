fa67062352cea717f3123ce3fa2c923d0f004e08
razzak118348 <abdurrazzak118348@gmail.com>
2024-09-09 14:12:18 +0600

    footer

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Root = () => {
 
 
     return (
-        <div className='mx-5  max-w-7xl lg:mx-auto font-poppins'>
+       <div>
+         <div className='mx-5  max-w-7xl lg:mx-auto font-poppins'>
             <Outlet></Outlet>
         </div>
+
+        <footer className="footer bg-base-200 text-base-content p-10">
+  <nav>
+    <h6 className="footer-title">Services</h6>
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Company</h6>
+    <Link to={'/about'} className="link link-hover">About us</Link>
+    <a className="link link-hover">Contact</a>
+    <Link to={'/career'} className="link link-hover">Career</Link>
+    <a className="link link-hover">Press kit</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Legal</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+  <form>
+    <h6 className="footer-title">Newsletter</h6>
+    <fieldset className="form-control w-80">
+      <label className="label">
+        <span className="label-text">Enter your email address</span>
+      </label>
+      <div className="join">
+        <input
+          type="text"
+          placeholder="username@site.com"
+          className="input input-bordered join-item" />
+        <button className="btn btn-primary join-item">Subscribe</button>
+      </div>
+    </fieldset>
+  </form>
+</footer>
+       </div>
     );
 };
 
